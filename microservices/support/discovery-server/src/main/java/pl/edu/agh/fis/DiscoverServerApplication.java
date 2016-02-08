554348d5dc006d4468593d3e2f8e818812cc091a
Wemstar <sylwestermacura@gmail.com>
2016-02-08 21:33:14 +0100

    Poprawka konfiguracji

--- microservices/support/discovery-server/src/main/java/pl/edu/agh/fis/DiscoverServerApplication.java
+++ microservices/support/discovery-server/src/main/java/pl/edu/agh/fis/DiscoverServerApplication.java
@@ -3,9 +3,7 @@ package pl.edu.agh.fis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoverServerApplication {
