1d41c68749c7f443026266b9d9848da20296540d
Wemstar <sylwestermacura@gmail.com>
2016-06-27 19:21:56 +0200

    Zmiana uprawnien gradlew

---
+++ commons/dto-service/src/main/java/pl/edu/agh/fis/dto/user/UserDTO.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.fis.dto.user;
+
+/**
+ * Created by wemstar on 2016-02-27.
+ */
+public class UserDTO {
+}
---
+++ microservices/support/edge-server/src/main/java/pl/edu/agh/fis/JedisCOnfig.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.fis;
+
+/**
+ * Created by wemstar on 2016-05-08.
+ */
+public class JedisCOnfig {
+}
